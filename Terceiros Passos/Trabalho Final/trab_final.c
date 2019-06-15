258f49199b990dc67b7fca75c352e153dc52510f
JavelFreitas <javelqueiroz@outlook.com>
2019-06-15 18:03:12 -0300

    Adições a estrutura principal

--- Terceiros Passos/Trabalho Final/trab_final.c
+++ Terceiros Passos/Trabalho Final/trab_final.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 #include "arvore.h"
 
 typedef struct jogador{
     Arvore *baralho;
-    char nome[30];
     int pontuacao;
     int vitorias;
 }Jogador;
@@ -16,7 +16,7 @@ typedef struct baralho{
 Baralho *criarBaralho(){
     Baralho *novo = (Baralho*)malloc(sizeof(Baralho));
     int i = 0;
-    for(i = 0; i<12 ;i++){
+    for(i = 0; i<13 ;i++){
         novo->cartas[i] = 0;
     }
     return novo;
@@ -25,10 +25,25 @@ Baralho *criarBaralho(){
 Jogador *criarJogador(){
     Jogador *novo_jogador = (Jogador*)malloc(sizeof(Jogador));
     novo_jogador->baralho = criar();
-    novo_jogador->nome[0] = "";
     novo_jogador->pontuacao = 0;
     novo_jogador->vitorias = 0;
     return novo_jogador;
+}
+
+void darCarta(Jogador *jog_, Baralho *embaralho){ //NOTE Criado com o intuito de reduzir o uso da mesma lógica em diferentes partes do código
+    int carta = ((rand() % 13) + 1);
+        while(carta != 0){
+            if(embaralho->cartas[carta-1] < 4){
+                inserir(jog_->baralho, carta);
+                embaralho->cartas[carta-1]++;
+                carta = 0;
+                return;
+            }
+            carta++;
+            if(carta == 13){
+                carta = 1;
+            }
+        }
 }
 
 void embaralhar(Jogador *jog_first, Jogador *jog_sec, Jogador *jog_third){
@@ -37,36 +52,21 @@ void embaralhar(Jogador *jog_first, Jogador *jog_sec, Jogador *jog_third){
     Baralho *embaralho = criarBaralho();
     
     while(qtd_cartas_jogador != 10){
-        carta = ((rand() % 13) + 1);
-        while(carta != 0){
-            if(embaralho->cartas[carta] < 4){
-                inserir(jog_first->baralho, carta);
-                embaralho->cartas[carta]++;
-                carta = 0;
-            }
-            carta++;
-            if(carta == 13)
-        }
-        
-
-
-
+        darCarta(jog_first, embaralho);
+        darCarta(jog_sec, embaralho);
+        darCarta(jog_third, embaralho);
         qtd_cartas_jogador++;
     }
-    
-
-
-
 }
 
 Jogador *vencedor(Jogador *jog_first, Jogador *jog_sec, Jogador *jog_third){
     
 }
 
-void rodada(Jogador *jog_first, Jogador *jog_sec, Jogador *jog_third){  // fixo 10 rodadas, uma por carta;
-    int carta_um = remover(jog_first->baralho, buscarMenor()); // falta criar essa estrutura pra retornar o menor valor da árvore;
-    int carta_dois = remover(jog_sec->baralho, buscarMenor());
-    int carta_tres = remover(jog_third->baralho, buscarMenor());
+void rodada(Jogador *jog_first, Jogador *jog_sec, Jogador *jog_third){  //NOTE  fixo 10 rodadas, uma por carta;
+    int carta_um = remover(jog_first->baralho, buscarMenor(jog_first->baralho)); //NOTE  falta criar essa estrutura pra retornar o menor valor da árvore;
+    int carta_dois = remover(jog_sec->baralho, buscarMenor(jog_sec->baralho));
+    int carta_tres = remover(jog_third->baralho, buscarMenor(jog_third->baralho));
     if(carta_um >= carta_dois && carta_um >= carta_tres){
         jog_first->pontuacao++;
     }
@@ -85,12 +85,14 @@ void partida(Jogador *jog_first, Jogador *jog_sec, Jogador *jog_third){
     while(--rodadas){
         rodada(jog_first, jog_sec, jog_third);
     }
+    //TODO seção onde escolhe o vencedor da partida
+    //TODO atribuir 0 a pontuação dos jogadores
 }
 
 void menu(){
     printf("Opções do jogo\n");
     printf("1 - Menu\n");
-    printf("2 - Jogar o jogo _numeroDeRodadas\n"); // numero maximo de rodadas?
+    printf("2 - Jogar o jogo _numeroDeRodadas\n"); //NOTE  numero maximo de rodadas?
     
 }
 
@@ -116,7 +118,8 @@ void jogo(){
             while(partidas > 0){
                 partida(jog_first, jog_sec, jog_third);
                 partidas--;
-            } // Cada jogador tem sua pontuação, mas não se sabe o vencedor
+            } //NOTE  Cada jogador tem sua pontuação, mas não se sabe o vencedor
+              //TODO seção onde escolhe o vencedor do jogo e mostra na tela
 
 
 
@@ -137,7 +140,7 @@ void jogo(){
 
 
 int main(){
-
+    srand(time(NULL));
 
     return 0;
 }
